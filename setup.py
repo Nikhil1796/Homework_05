fdd1f158d9eba259bfd7baee53509bfc7fcc903c
Nikhil1796 <n1503010138@gmail.com>
2022-12-03 14:27:58 +0100

    its my first git hub project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Homework_05',
+    version='1.0.0',
+    packages=['snowflake'],
+    url='https://github.com/Nikhil1796/Homework_05.git',
+    license='',
+    author='Nikhil&A',
+    author_email='',
+    description='Python package for generating colorful snowflakes using Python'
+)
